5bd61476413ec41dc264b8260451cbe44ecdbbe7
jay <cleve703@gmail.com>
2020-06-08 16:04:18 -0400

    add kitten model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+kittens = Kitten.create([
+  {name: 'Sylvester', age: 4, cuteness: 3, softness: 5}, 
+  {name: 'Fluffy', age: 1, cuteness: 8, softness: 10}, 
+  {name: 'Snowball', age: 2, cuteness: 9, softness: 9}, 
+  {name: 'Garfield', age: 3, cuteness: 6, softness: 5}
+  ])
